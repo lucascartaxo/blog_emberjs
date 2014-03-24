8df1b6414cefa44431ab6659b43a5d029cfeb53c
Lucas Cartaxo <lucascartaxor@gmail.com>
2014-03-24 18:29:03 -0300

    basic routes and templates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 var App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
+
+App.Router.map(function() {
+  this.resource("about");
+  this.resource("posts");
+});
+
+App.Router.reopen({
+  // location: 'none'
+});
