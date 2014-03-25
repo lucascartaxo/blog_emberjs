114a448074bf94e7d3aa01753281e5a87cd3bedf
Lucas Cartaxo <lucascartaxor@gmail.com>
2014-03-25 10:26:36 -0300

    editing posts

--- js/app.js
+++ js/app.js
@@ -4,13 +4,28 @@ var App = Ember.Application.create({
 
 App.Router.map(function() {
   this.resource("about");
-  this.resource("posts");
-  this.resource("post", { path: ":post_id"});
+  this.resource("posts", function () {
+    this.resource("post", { path: ":post_id"});
+  });
 });
 
-App.PostsRoute = Em.Route.extend({
+App.PostsRoute = Ember.Route.extend({
   model: function() {
       return posts;
+  }
+});
+
+App.PostController = Ember.ObjectController.extend({
+  isEditing: false,
+
+  actions: {
+    edit: function () {
+      this.set("isEditing", true);
+    },
+    doneEditing: function () {
+      this.set("isEditing", false);
+    },
+
   }
 });
 
